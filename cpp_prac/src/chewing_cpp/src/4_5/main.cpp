0bab388f44eaf19df66e75e28286ce125d3e66d9
Hyeokjin Yun <yhjin1096@gamil.com>
2024-07-24 00:48:47 +0900

    chewing_cpp 4_6

--- cpp_prac/src/chewing_cpp/src/4_5/main.cpp
+++ cpp_prac/src/chewing_cpp/src/4_5/main.cpp
@@ -1,110 +1,4 @@
-#include <iostream>
-#include <cstring>
-class MyString
-{
-    private:
-        char* string_content;
-        int string_length;
-        int memory_capacity;
-    public:
-        MyString(char c)
-        {
-            string_content = new char[1];
-            string_content[0] = c;
-            string_length = 1;
-            memory_capacity = 1;
-        }
-        MyString(const char* str)
-        {
-            string_length = strlen(str);
-            memory_capacity = string_length;
-            string_content = new char[string_length];
-            
-            for(int i = 0; i != string_length; i++)
-                string_content[i] = str[i];
-        }
-        MyString(const MyString& str) // 복사 생성자
-        {
-            string_length = str.string_length;
-            memory_capacity = str.string_length;
-            string_content = new char[string_length];
-            
-            for(int i = 0; i != string_length; i++)
-                string_content[i] = str.string_content[i];
-        }
-        ~MyString() { delete[] string_content; }
-
-        //class 멤버 변수 변경x -> const(상수) 함수
-        int length() const { return string_length; }
-
-        void print() const
-        {
-            for (int i = 0; i != string_length; i++)
-                std::cout << string_content[i];
-        }
-
-        void println() const
-        {
-            for (int i = 0; i != string_length; i++)
-                std::cout << string_content[i];
-            std::cout << std::endl;
-        }
-
-        //assign 함수, = 연산과 동일
-        MyString& assign(const MyString& str)
-        {
-            if(str.length() > memory_capacity)
-            {
-                delete[] string_content;
-                string_content = new char[str.length()];
-                memory_capacity = str.string_length;
-            }
-
-            for(int i = 0; i != string_length; i++)
-                string_content[i] = str.string_content[i];
-            
-            string_length = str.length();
-
-            return *this;
-        }
-        MyString& assign(const char* str)
-        {
-            int str_length = strlen(str);
-            if (str_length > memory_capacity)
-            {
-                delete[] string_content;
-                string_content = new char[str_length];
-                memory_capacity = str_length;
-            }
-            for (int i = 0; i != str_length; i++)
-                string_content[i] = str[i];
-            
-            string_length = str_length;
-            
-            return *this;
-        }
-        int capacity() { return memory_capacity; }
-        void reserve(int size)
-        {
-            if(size > memory_capacity)
-            {
-                char* prev_string_content = string_content;
-                string_content = new char[size];
-                memory_capacity = size;
-                for(int i = 0; i != string_length; i++)
-                    string_content[i] = prev_string_content[i];
-                
-                // delete[] prev_string_content;
-            }
-        }
-        char at(int i)
-        {
-            if(i >= string_length || i <0)
-                return NULL;
-            else
-                return string_content[i];
-        }
-};
+#include "chewing_cpp/4_5/MyString.hpp"
 
 int main(int argc, char** argv)
 {
@@ -128,6 +22,18 @@ int main(int argc, char** argv)
 
         std::cout << "Capacity : " << str1.capacity() << std::endl;
         std::cout << "String length : " << str1.length() << std::endl;
+        str1.println();
+    }
+    {
+        MyString str1("very long string");
+        MyString str2("<some string inserted between>");
+        str1.reserve(30);
+
+        std::cout << "Capacity : " << str1.capacity() << std::endl;
+        std::cout << "String length : " << str1.length() << std::endl;
+        str1.println();
+
+        str1.insert(5, str2);
         str1.println();
     }
     return 0;
---
+++ cpp_prac/src/chewing_cpp/src/4_6/main.cpp
@@ -0,0 +1,48 @@
+#include "chewing_cpp/4_5/MyString.hpp"
+
+void DoSomethingWithString(MyString s)
+{
+  // Do something...
+  s.println();
+};
+
+class A {
+    private:
+        mutable int data_;
+
+    public:
+        A(int data) : data_(data) {}
+        void DoSomething(int x) const {
+            data_ = x;  // 가능!
+        }
+
+    void PrintData() const { std::cout << "data: " << data_ << std::endl; }
+};
+
+int main(int argc, char** argv)
+{
+    {
+        MyString s(3);
+    }
+    {
+        DoSomethingWithString(MyString("abc"));
+        DoSomethingWithString("abc"); //암시적 변환
+    }
+    {
+        //암시적 변환 막기 - explicit
+        DoSomethingWithString(MyString('c'));
+        // DoSomethingWithString('c');
+    }
+    {
+        MyString s(5);
+        // MyString ss = 5;
+    }
+    ///////////////////////////////////////////////////
+    {
+        A a(10);
+        a.PrintData();
+        a.DoSomething(3);
+        a.PrintData();
+    }
+    return 0;
+}
