8966883f2110d7c457cb19f096c4f6381a56a148
Hyeokjin Yun <yhjin1096@gmail.com>
2025-05-09 20:45:09 +0900

    optimize done

--- local_ba_g2o/src/local_ba_g2o/include/local_ba_g2o/frame.hpp
+++ local_ba_g2o/src/local_ba_g2o/include/local_ba_g2o/frame.hpp
@@ -10,11 +10,13 @@ class Frame
         Frame(const Vec2_t& xy, unsigned int id, int num_landmarks)
         {
             // yaw를 -90도로 설정하고 z를 1.6m로 고정
-            pose_ = Mat44_t::Identity();
-            pose_.block<3,3>(0,0) = Eigen::AngleAxisd(-M_PI/2.0, Eigen::Vector3d::UnitZ()).toRotationMatrix();
-            pose_(0,3) = xy(0);
-            pose_(1,3) = xy(1);
-            pose_(2,3) = 1.6;
+            pose_w2c_ = Mat44_t::Identity();
+            pose_w2c_.block<3,3>(0,0) = Eigen::AngleAxisd(-M_PI/2.0, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+            pose_w2c_(0,3) = xy(0);
+            pose_w2c_(1,3) = xy(1);
+            pose_w2c_(2,3) = 1.6;
+
+            pose_c2w_ = pose_w2c_.inverse();
 
             id_ = id;
             intrinsic_ << 600, 0, 320,
@@ -33,13 +35,21 @@ class Frame
             observations_[landmark_id] = observation;
         }
 
+        void addGTObservation(unsigned int landmark_id, const Vec2_t& observation)
+        {
+            gt_observations_[landmark_id] = observation;
+        }
+
+        
+
         cv::Mat gt_image_, observed_image_;
-        Mat44_t pose_;
+        Mat44_t pose_w2c_, pose_c2w_;
         int id_ = 0;
 
         Mat33_t intrinsic_;
         
         std::unordered_map<unsigned int, Vec2_t> observations_;
+        std::unordered_map<unsigned int, Vec2_t> gt_observations_;
 };
 
 #endif
--- local_ba_g2o/src/local_ba_g2o/src/main.cpp
+++ local_ba_g2o/src/local_ba_g2o/src/main.cpp
@@ -13,10 +13,9 @@
 #include <local_ba_g2o/random.hpp>
 #include <local_ba_g2o/frame.hpp>
 
-#include <tinycolormap.hpp>
+#include <optimize/optimizer.hpp>
 
-
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr convertLandmarksToPointCloud(const std::vector<Landmark>& landmarks, std::vector<tinycolormap::Color>& colors) {
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr convertLandmarksToPointCloud(const std::vector<Landmark>& landmarks, cv::Scalar color) {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     cloud->points.resize(landmarks.size());
 
@@ -32,14 +31,43 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr convertLandmarksToPointCloud(const std::v
         cloud->points[i].y = landmarks[i].pos_(1);
         cloud->points[i].z = landmarks[i].pos_(2);
 
-        double normalized_z = (landmarks[i].pos_(2) - min_z) / (max_z - min_z);
+        cloud->points[i].r = static_cast<uint8_t>(color(2));
+        cloud->points[i].g = static_cast<uint8_t>(color(1));
+        cloud->points[i].b = static_cast<uint8_t>(color(0));
+    }
+    cloud->width = landmarks.size();
+    cloud->height = 1;
+    cloud->is_dense = false;
 
-        auto color = tinycolormap::GetColor(normalized_z, tinycolormap::ColormapType::Heat);
-        colors.push_back(color);
+    return cloud;
+}
 
-        cloud->points[i].r = static_cast<uint8_t>(color.r() * 255);
-        cloud->points[i].g = static_cast<uint8_t>(color.g() * 255);
-        cloud->points[i].b = static_cast<uint8_t>(color.b() * 255);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr convertLandmarksToPointCloud(const Optimizer& optimizer,
+                                                                const std::vector<Landmark>& landmarks,
+                                                                int num_frames) {
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    cloud->points.resize(landmarks.size());
+
+    double min_z = std::numeric_limits<double>::max();
+    double max_z = std::numeric_limits<double>::lowest();
+    for(const auto& landmark : landmarks) {
+        min_z = std::min(min_z, landmark.pos_(2));
+        max_z = std::max(max_z, landmark.pos_(2));
+    }
+
+    for(size_t i = 0; i < landmarks.size(); ++i) {
+        auto vertex_it = optimizer.optimizer_.vertices().find(i + num_frames);
+        if(vertex_it != optimizer.optimizer_.vertices().end()) {
+            g2o::VertexPointXYZ* vtx = dynamic_cast<g2o::VertexPointXYZ*>(vertex_it->second);
+            if(vtx) {
+                cloud->points[i].x = vtx->estimate()(0);
+                cloud->points[i].y = vtx->estimate()(1);
+                cloud->points[i].z = vtx->estimate()(2);
+                cloud->points[i].r = static_cast<uint8_t>(255);
+                cloud->points[i].g = static_cast<uint8_t>(255);
+                cloud->points[i].b = static_cast<uint8_t>(0);
+            }
+        }
     }
     cloud->width = landmarks.size();
     cloud->height = 1;
@@ -64,12 +92,12 @@ void publishFramePose(const Frame& frame, tf2_ros::TransformBroadcaster& broadca
     transform.child_frame_id = "frame_" + std::to_string(frame.id_);
 
     // pose에서 translation 추출
-    transform.transform.translation.x = frame.pose_(0,3);
-    transform.transform.translation.y = frame.pose_(1,3);
-    transform.transform.translation.z = frame.pose_(2,3);
+    transform.transform.translation.x = frame.pose_w2c_(0,3);
+    transform.transform.translation.y = frame.pose_w2c_(1,3);
+    transform.transform.translation.z = frame.pose_w2c_(2,3);
 
     // pose에서 rotation 추출 (rotation matrix를 quaternion으로 변환)
-    Eigen::Matrix3d rot = frame.pose_.block<3,3>(0,0);
+    Eigen::Matrix3d rot = frame.pose_w2c_.block<3,3>(0,0);
     Eigen::Quaterniond q(rot);
     transform.transform.rotation.x = q.x();
     transform.transform.rotation.y = q.y();
@@ -79,8 +107,38 @@ void publishFramePose(const Frame& frame, tf2_ros::TransformBroadcaster& broadca
     broadcaster.sendTransform(transform);
 }
 
-cv::Mat concat_observed_image, concat_gt_image, concat_images;
-void setImages(std::vector<Frame>& frames, const std::vector<Landmark>& landmarks, const std::vector<tinycolormap::Color>& colors)
+void publishFramePoses(const Optimizer& optimizer, tf2_ros::TransformBroadcaster& broadcaster, int num_frames)
+{
+    for(int i = 0; i < num_frames; ++i) {
+        auto vertex_it = optimizer.optimizer_.vertices().find(i);
+        if(vertex_it != optimizer.optimizer_.vertices().end()) {
+            g2o::VertexSE3Expmap* vtx = dynamic_cast<g2o::VertexSE3Expmap*>(vertex_it->second);
+            if(vtx) {
+                geometry_msgs::TransformStamped transform;
+                transform.header.stamp = ros::Time::now();
+                transform.header.frame_id = "map";
+                transform.child_frame_id = "optimal_frame_" + std::to_string(i);
+
+                // pose에서 translation 추출
+                g2o::SE3Quat pose_inv = vtx->estimate().inverse();
+                transform.transform.translation.x = pose_inv.translation()(0);
+                transform.transform.translation.y = pose_inv.translation()(1);
+                transform.transform.translation.z = pose_inv.translation()(2);
+
+                // pose에서 rotation 추출 (rotation matrix를 quaternion으로 변환)
+                Eigen::Quaterniond q = pose_inv.rotation();
+                transform.transform.rotation.x = q.x();
+                transform.transform.rotation.y = q.y();
+                transform.transform.rotation.z = q.z();
+                transform.transform.rotation.w = q.w();
+
+                broadcaster.sendTransform(transform);
+            }
+        }
+    }
+}
+
+void setObservations(std::vector<Frame>& frames, const std::vector<Landmark>& gt_landmarks, const std::vector<Landmark>& noise_landmarks)
 {
     for(auto& frame : frames)
     {
@@ -89,54 +147,99 @@ void setImages(std::vector<Frame>& frames, const std::vector<Landmark>& landmark
         frame.gt_image_ = cv::Mat::zeros(height, width, CV_8UC3);
         frame.observed_image_ = cv::Mat::zeros(height, width, CV_8UC3);
 
-        for(const auto& landmark : landmarks)
+        for(const auto& landmark : gt_landmarks)
         {
-            Eigen::Vector4d point_world(landmark.pos_(0), landmark.pos_(1), landmark.pos_(2), 1.0);
-            Eigen::Vector4d point_frame = frame.pose_.inverse() * point_world;
+            Eigen::Vector4d gt_point_world(landmark.pos_(0), landmark.pos_(1), landmark.pos_(2), 1.0);
+            Eigen::Vector4d gt_point_frame = frame.pose_w2c_.inverse() * gt_point_world;
 
-            if(point_frame(2) <= 0) continue;
+            if(gt_point_frame(2) <= 0) continue;
 
-            int x = static_cast<int>(point_frame(0) * frame.intrinsic_(0,0) / point_frame(2) + frame.intrinsic_(0,2));
-            int y = static_cast<int>(point_frame(1) * frame.intrinsic_(1,1) / point_frame(2) + frame.intrinsic_(1,2));
-            int noise_x = x + Random::GaussRand(0, 1);
-            int noise_y = y + Random::GaussRand(0, 1);
+            int gt_x = static_cast<int>(gt_point_frame(0) * frame.intrinsic_(0,0) / gt_point_frame(2) + frame.intrinsic_(0,2));
+            int gt_y = static_cast<int>(gt_point_frame(1) * frame.intrinsic_(1,1) / gt_point_frame(2) + frame.intrinsic_(1,2));
 
-            if(x < 0 || x >= width || y < 0 || y >= height) continue;
+            if(gt_x < 0 || gt_x >= width || gt_y < 0 || gt_y >= height) continue;
 
-            int r = colors[landmark.id_].r()*255;
-            int g = colors[landmark.id_].g()*255;
-            int b = colors[landmark.id_].b()*255;
+            frame.addGTObservation(landmark.id_, Vec2_t(gt_x, gt_y));
 
-            // frame.gt_image_.at<cv::Vec3b>(y, x) = cv::Vec3b(colors[landmark.id_].b()*255, colors[landmark.id_].g()*255, colors[landmark.id_].r()*255);
-            cv::circle(frame.gt_image_, cv::Point(x, y), 2, cv::Scalar(b, g, r), -1);
+            int noise_x = static_cast<int>(gt_x + Random::GaussRand(0.0, 1.0));
+            int noise_y = static_cast<int>(gt_y + Random::GaussRand(0.0, 1.0));
 
             if(noise_x < 0 || noise_x >= width || noise_y < 0 || noise_y >= height) continue;
-
-            cv::circle(frame.observed_image_, cv::Point(noise_x, noise_y), 2, cv::Scalar(b, g, r), -1);
 
             frame.addObservation(landmark.id_, Vec2_t(noise_x, noise_y));
         }
     }
+}
 
-    concat_gt_image = frames[0].gt_image_.clone();
-    cv::Mat line_image = cv::Mat(concat_gt_image.rows, 1, CV_8UC3, cv::Scalar(0, 0, 255));
-    for(size_t i = 1; i < frames.size(); ++i)
-    {
-        cv::hconcat(concat_gt_image, line_image, concat_gt_image);
-        cv::hconcat(concat_gt_image, frames[i].gt_image_, concat_gt_image);
-    }
-    cv::line(concat_gt_image, cv::Point(0, concat_gt_image.rows/2), cv::Point(concat_gt_image.cols, concat_gt_image.rows/2), cv::Scalar(255, 255, 255), 1);
+void observedImage(const Optimizer& optimizer, std::vector<Frame>& frames, const std::vector<Landmark>& landmarks)
+{
+    cv::Mat concat_image;
+    bool is_first = true;
 
-    concat_observed_image = frames[0].observed_image_.clone();
-    for(size_t i = 1; i < frames.size(); ++i)
+    for(auto& frame : frames)
     {
-        cv::hconcat(concat_observed_image, line_image, concat_observed_image);
-        cv::hconcat(concat_observed_image, frames[i].observed_image_, concat_observed_image);
+        frame.gt_image_ = cv::Mat::zeros(frame.intrinsic_(1,2) * 2, frame.intrinsic_(0,2) * 2, CV_8UC3);
+        frame.observed_image_ = cv::Mat::zeros(frame.intrinsic_(1,2) * 2, frame.intrinsic_(0,2) * 2, CV_8UC3);
+        cv::Mat line_image = cv::Mat(frame.gt_image_.rows, 1, CV_8UC3, cv::Scalar(255, 255, 255));
+
+        for(const auto& observation : frame.gt_observations_)
+        {
+            int x = observation.second(0);
+            int y = observation.second(1);
+            if(x >= 0 && x < frame.gt_image_.cols && y >= 0 && y < frame.gt_image_.rows)
+                cv::circle(frame.gt_image_, cv::Point(x, y), 1, cv::Scalar(0, 255, 0), -1);
+        }
+
+        for(const auto& observation : frame.observations_)
+        {
+            int x = observation.second(0);
+            int y = observation.second(1);
+            if(x >= 0 && x < frame.observed_image_.cols && y >= 0 && y < frame.observed_image_.rows)
+                cv::circle(frame.observed_image_, cv::Point(x, y), 1, cv::Scalar(0, 0, 255), -1);
+        }
+
+        cv::Mat whole_image;
+        cv::add(frame.gt_image_, frame.observed_image_, whole_image);
+
+        for(const auto& landmark : landmarks)
+        {
+            auto vertex_it = optimizer.optimizer_.vertices().find(landmark.id_ + frames.size());
+            if(vertex_it != optimizer.optimizer_.vertices().end())
+            {
+                g2o::VertexPointXYZ* vtx = dynamic_cast<g2o::VertexPointXYZ*>(vertex_it->second);
+                if(vtx)
+                {
+                    // 3D -> 2D 투영
+                    Eigen::Vector4d point_world(vtx->estimate()(0), vtx->estimate()(1), vtx->estimate()(2), 1.0);
+                    Eigen::Vector4d point_frame = frame.pose_w2c_.inverse() * point_world;
+
+                    if(point_frame(2) > 0)  // 카메라 앞에 있는 경우만
+                    {
+                        int x = static_cast<int>(point_frame(0) * frame.intrinsic_(0,0) / point_frame(2) + frame.intrinsic_(0,2));
+                        int y = static_cast<int>(point_frame(1) * frame.intrinsic_(1,1) / point_frame(2) + frame.intrinsic_(1,2));
+
+                        if(x >= 0 && x < whole_image.cols && y >= 0 && y < whole_image.rows)
+                            cv::circle(whole_image, cv::Point(x, y), 1, cv::Scalar(0, 255, 255), -1);
+                    }
+                }
+            }
+        }
+
+        if(is_first)
+        {
+            concat_image = whole_image.clone();
+            is_first = false;
+        }
+        else
+        {
+            cv::hconcat(concat_image, line_image, concat_image);
+            cv::hconcat(concat_image, whole_image, concat_image);
+        }
     }
-    cv::line(concat_observed_image, cv::Point(0, concat_observed_image.rows/2), cv::Point(concat_observed_image.cols, concat_observed_image.rows/2), cv::Scalar(255, 255, 255), 1);
-    cv::vconcat(concat_gt_image, concat_observed_image, concat_images);
-    cv::line(concat_images, cv::Point(0, concat_images.rows/2), cv::Point(concat_images.cols, concat_images.rows/2), cv::Scalar(0, 0, 255), 1);
-    cv::resize(concat_images, concat_images, cv::Size(concat_images.cols/2, concat_images.rows/2));
+
+    // 이미지 크기 조정 및 표시
+    // cv::resize(concat_image, concat_image, cv::Size(), 0.5, 0.5);
+    cv::imshow("observed image", concat_image);
 }
 
 int main(int argc, char **argv)
@@ -146,38 +249,61 @@ int main(int argc, char **argv)
 
     int num_landmarks = nh.param<int>("num_landmarks", 100);
     int num_frames = nh.param<int>("num_frames", 5);
+    int num_iter = nh.param<int>("num_iter", 30);
 
-    //landmarks
-    std::vector<tinycolormap::Color> colors;
-    std::vector<Landmark> landmarks = Random::getRandomLandmarks(num_landmarks);
-    ros::Publisher landmarks_pub = nh.advertise<sensor_msgs::PointCloud2>("/landmarks", 1);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud = convertLandmarksToPointCloud(landmarks, colors);
+    ros::Publisher gt_landmarks_pub = nh.advertise<sensor_msgs::PointCloud2>("/gt_landmarks", 1);
+    ros::Publisher noise_landmarks_pub = nh.advertise<sensor_msgs::PointCloud2>("/observed_landmarks", 1);
+    ros::Publisher optimal_landmarks_pub = nh.advertise<sensor_msgs::PointCloud2>("/optimized_landmarks", 1);
 
-    //frames
-    std::vector<Frame> frames;
-    for(int i = 0; i < num_frames; ++i)
-        frames.push_back(Frame(Vec2_t(0.3 * i, 0.0), i, num_landmarks));
     tf2_ros::TransformBroadcaster tf_broadcaster;
 
-    setImages(frames, landmarks, colors);
+    //optimizer
+    Optimizer optimizer;
 
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(5);
     while(ros::ok())
     {
+        int count = 0;
+        optimizer.reset();
 
-        //publish
-        publishLandmarks(cloud, landmarks_pub);
-        for(const auto& frame : frames) {
-            publishFramePose(frame, tf_broadcaster);
+        //landmarks
+        std::vector<Landmark> gt_landmarks = Random::getRandomLandmarks(num_landmarks);
+        std::vector<Landmark> noise_landmarks = gt_landmarks;
+        for(auto& landmark : noise_landmarks)
+            landmark.pos_ += Vec3_t(Random::UniformRand(-0.2, 0.2), Random::UniformRand(-0.2, 0.2), Random::UniformRand(-0.2, 0.2));
+
+        //frames
+        std::vector<Frame> frames;
+        for(int i = 0; i < num_frames; ++i)
+            frames.push_back(Frame(Vec2_t(0.8 * i, 0.0), i, num_landmarks));
+        setObservations(frames, gt_landmarks, noise_landmarks);
+
+        if(!optimizer.setup(frames, noise_landmarks))
+            break;
+
+        while(count < num_iter)
+        {
+            //publish landmarks
+            publishLandmarks(convertLandmarksToPointCloud(gt_landmarks, cv::Scalar(0, 255, 0)), gt_landmarks_pub);
+            publishLandmarks(convertLandmarksToPointCloud(noise_landmarks, cv::Scalar(0, 0, 255)), noise_landmarks_pub);
+            publishLandmarks(convertLandmarksToPointCloud(optimizer, noise_landmarks, num_frames), optimal_landmarks_pub);
+
+            //publish frame poses
+            // for(auto& frame : frames)
+            //     publishFramePose(frame, tf_broadcaster);
+            publishFramePoses(optimizer, tf_broadcaster, num_frames);
+
+            //imshow
+            observedImage(optimizer, frames, gt_landmarks);
+            cv::waitKey(1);
+
+            //optimize
+            optimizer.optimize(1);
+
+            count++;
+            loop_rate.sleep();
+            ros::spinOnce();
         }
-
-        // cv::imshow("concat_gt_image", concat_gt_image);
-        // cv::imshow("concat_observed_image", concat_observed_image);
-        cv::imshow("concat_images", concat_images);
-        cv::waitKey(1);
-
-        ros::spinOnce();
-        loop_rate.sleep();
     }
 
     return 0;
